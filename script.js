01cfb94e09c5c9933c74962d5a8c782de082c237
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-10-23 12:13:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
+//const getSumBtn = document.createElement("button");
+//getSumBtn.append("Get Total Price");
+//document.body.appendChild(getSumBtn);
+
+//const getSum = () => {
+//Add your code here
+  
+//};
+
+//getSumBtn.addEventListener("click", getSum);
+
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+    // Select all elements with the class "prices" in the grocery list
+    const priceElements = document.querySelectorAll(".prices");
+
+    let totalPrice = 0;
+
+    // Calculate the total price by summing up the prices
+    priceElements.forEach(priceElement => {
+        totalPrice += parseFloat(priceElement.textContent);
+    });
+
+    // Create a new row to display the total price
+    const totalRow = document.createElement("tr");
+    const totalCell1 = document.createElement("td");
+    const totalCell2 = document.createElement("td");
+    totalCell1.textContent = "Total Price:";
+    totalCell2.textContent = totalPrice.toFixed(2);
+    totalRow.appendChild(totalCell1);
+    totalRow.appendChild(totalCell2);
+
+    // Check if the total row already exists and replace it
+    const existingTotalRow = document.querySelector("#total-price");
+    if (existingTotalRow) {
+        existingTotalRow.replaceWith(totalRow);
+    } else {
+        const tableFooter = document.querySelector("table tfoot");
+        tableFooter.appendChild(totalRow);
+    }
 };
 
 getSumBtn.addEventListener("click", getSum);
+
+// Initial calculation when the page loads
+getSum();
 
